4d429e51f7012a72fa587f87644fea1d6e8bc491
The Bridge Scorekeeper <35344976+thebridsk@users.noreply.github.com>
2018-01-20 22:58:06 -0500

    zip store

---
+++ server/src/main/scala/com/example/backend/BridgeServiceZipStore.scala
@@ -0,0 +1,101 @@
+package com.example.backend
+
+import com.example.data.LoggerConfig
+import com.example.backend.resource.StoreSupport
+import com.example.data.Team
+import com.example.data.MatchChicago
+import com.example.data.MatchDuplicate
+import com.example.data.MatchDuplicateResult
+import com.example.data.Hand
+import com.example.data.Board
+import com.example.data.Table
+import com.example.backend.resource.FileStore
+import scala.reflect.io.Directory
+import com.example.backend.resource.MultiStore
+import com.example.backend.resource.JavaResourceStore
+import com.example.data.BoardSet
+import com.example.data.Movement
+import com.example.data.MatchRubber
+import com.example.data.MatchDuplicateV1
+import utils.logging.Logger
+import com.example.data.MatchChicagoV1
+import com.example.data.MatchChicagoV2
+import com.example.backend.resource.VersionedInstanceJson
+import com.example.data.MatchDuplicateV2
+import com.example.backend.resource.FileIO
+import com.example.backend.resource.JsonYamlConverter
+import com.example.backend.resource.JsonConverter
+import com.example.backend.resource.YamlConverter
+import play.api.libs.json._
+import com.example.yaml.YamlSupport._
+import com.example.data.Id
+import scala.concurrent.ExecutionContext
+import scala.reflect.io.File
+import com.example.backend.resource.ZipFileForStore
+import com.example.backend.resource.ZipStore
+import com.example.backend.resource.JavaResourcePersistentSupport
+import com.example.backend.resource.ZipPersistentSupport
+
+object BridgeServiceZipStore {
+
+  val log = Logger( getClass().getName )
+
+}
+
+/**
+ * An implementation of the backend for our service.
+ * This is used for testing, and is predefined with
+ * resources to facilitate testing.
+ * @author werewolf
+ */
+class BridgeServiceZipStore( val zipfilename: File,
+                             useYaml: Boolean = true
+                           )(
+                             implicit
+                               execute: ExecutionContext
+                           ) extends BridgeServiceWithLogging {
+  self =>
+
+  import BridgeServiceZipStore._
+
+  import scala.collection.mutable.Map
+
+  val bridgeResources = BridgeResources(useYaml,true,false,true)
+  import bridgeResources._
+
+  val zipfile = new ZipFileForStore(zipfilename)
+
+  val chicagos = ZipStore[Id.MatchDuplicate,MatchChicago](zipfile)
+  val duplicates = ZipStore[Id.MatchDuplicate,MatchDuplicate](zipfile)
+  val duplicateresults = ZipStore[Id.MatchDuplicateResult,MatchDuplicateResult](zipfile)
+  val rubbers = ZipStore[String,MatchRubber](zipfile)
+
+  val boardSets = MultiStore[String,BoardSet]( List(
+                                                    new ZipPersistentSupport(zipfile),
+                                                    new JavaResourcePersistentSupport("/com/example/backend/", "Boardsets.txt", self.getClass.getClassLoader)
+                                              ))
+
+  val movements = MultiStore[String,Movement]( List(
+                                                    new ZipPersistentSupport(zipfile),
+                                                    new JavaResourcePersistentSupport("/com/example/backend/", "Movements.txt", self.getClass.getClassLoader)
+                                              ))
+
+  /**
+   * closes the store.  Do not call other methods after calling this method.
+   */
+  def close() = {
+    zipfile.close()
+  }
+
+  /**
+   * closes and deletes the zip file.  Do not call other methods after calling this method.
+   * @return  <code>true</code> if zip file is successfully deleted; <code>false</code> otherwise
+   */
+  def delete() = {
+    zipfile.close()
+    zipfilename.delete()
+  }
+
+  override
+  def toString() = "BridgeServiceZipStore"
+}
---
+++ server/src/main/scala/com/example/backend/resource/ZipStore.scala
@@ -0,0 +1,192 @@
+package com.example.backend.resource
+
+import scala.concurrent.duration._
+import akka.http.scaladsl.model.StatusCodes
+import com.example.data.RestMessage
+import com.example.data.VersionedInstance
+import scala.reflect.io.Directory
+import utils.logging.Logger
+import scala.annotation.tailrec
+import java.io.IOException
+import scala.collection.JavaConverters._
+import ZipStore.log
+import scala.concurrent.ExecutionContext
+import scala.concurrent.Future
+import com.example.data.Id
+import java.util.zip.ZipFile
+import Implicits._
+
+object ZipStore {
+  val log = Logger[ZipStore[_,_]]
+
+  def apply[VId,VType <: VersionedInstance[VType,VType,VId]](
+                            zipfile: ZipFileForStore,
+                            cacheInitialCapacity: Int = 5,
+                            cacheMaxCapacity: Int = 100,
+                            cacheTimeToLive: Duration = 10.minutes,
+                            cacheTimeToIdle: Duration = 9.minutes,
+                          )(
+                            implicit
+                              cachesupport: StoreSupport[VId,VType],
+                              execute: ExecutionContext
+                          ): ZipStore[VId,VType] = {
+    new ZipStore(zipfile,cacheInitialCapacity, cacheMaxCapacity, cacheTimeToLive, cacheTimeToIdle )
+  }
+}
+
+class ZipPersistentSupport[VId,VType <: VersionedInstance[VType,VType,VId]](
+          val zipfile: ZipFileForStore
+        )(
+          implicit
+            support: StoreSupport[VId, VType],
+            execute: ExecutionContext
+        ) extends PersistentSupport[VId,VType] {
+
+  self =>
+
+  val resourceName = support.resourceName
+
+  /**
+   * Get all the IDs from persistent storage
+   */
+  def getAllIdsFromPersistent(cacheKeys: ()=>Set[VId]): Set[VId] = {
+    val pattern = (resourceName+"\\.([^.]+)\\..*").r
+
+    val keys = zipfile.entries().map { zipentry =>
+      zipentry.getName
+    }.flatMap {
+      case pattern(sid) =>
+        support.stringToId(sid)
+      case _ =>
+        None
+    }.toSet
+
+    log.finer(s"getAllIdsFromPersistent for ${resourceURI} returning ${keys}")
+
+    keys
+  }
+
+  /**
+   * Create an entry in the persistent store.
+   * @param v the value to create.  The id field is ignored and will be assigned.
+   * @return a future to the stored value, with the correct ID.
+   */
+  def createInPersistent(
+                          useId: Option[VId],
+                          v: VType
+                        ): Future[Result[VType]] = {
+    storeIsReadOnly.logit("ZipPersistentSupport.createInPersistent")
+  }
+
+  /**
+   * Read a resource from the persistent store
+   * @param id
+   * @return the result containing the resource or an error
+   */
+  def getFromPersistent(
+                         id: VId
+                       ): Future[Result[VType]] = {
+    Future( read(id) )
+  }
+
+  /**
+   * Read a resource from the persistent store
+   * @param id
+   * @return the result containing the resource or an error
+   */
+  def read( id: VId ): Result[VType] = self.synchronized {
+
+    @tailrec
+    def read( list: List[String] ): Result[VType] = {
+      if (list.isEmpty) notFound(id)
+      else {
+        val f = list.head
+
+        val ovt = try {
+                    zipfile.readFileSafe(f) match {
+                      case Some(v) =>
+                        val (goodOnDisk,vt) = support.fromJSON(v)
+                        Option(vt)
+                      case None =>
+                        None
+                    }
+                  } catch {
+                    case x: Exception =>
+                      log.info(s"Unable to read ${f}: ${x}")
+                      None
+                  }
+        ovt match {
+          case Some(vt) => Result(vt)
+          case None => read( list.tail )
+        }
+      }
+    }
+
+    read( readFilenames(id) )
+  }
+
+
+  /**
+   * Write a resource to the persistent store
+   * @param id
+   * @param v
+   * @return the result containing the resource or an error
+   */
+  def putToPersistent(
+                       id: VId,
+                       v: VType
+                     ): Future[Result[VType]] = {
+    storeIsReadOnly.logit("ZipPersistentSupport.putToPersistent")
+  }
+
+  /**
+   * Write a resource to the persistent store
+   * @param id
+   * @param v
+   * @return the result containing the old resource or an error
+   */
+  def deleteFromPersistent(
+                       id: VId,
+                       cacheValue: Option[VType]
+                     ): Future[Result[VType]] =  {
+    storeIsReadOnly.logit("ZipPersistentSupport.deleteFromPersistent")
+  }
+
+  def readFilenames( id: VId ) = {
+    support.getReadExtensions().map{ e =>
+      resourceName+"."+id+e
+    }
+  }
+
+}
+
+object ZipPersistentSupport {
+  def apply[VId,VType <: VersionedInstance[VType,VType,VId]](
+          zipfile: ZipFileForStore
+        )(
+          implicit
+            support: StoreSupport[VId, VType],
+            execute: ExecutionContext
+        ): ZipPersistentSupport[VId, VType] = {
+    new ZipPersistentSupport(zipfile)
+  }
+}
+
+class ZipStore[VId,VType <: VersionedInstance[VType,VType,VId]](
+                            val zipfile: ZipFileForStore,
+                            cacheInitialCapacity: Int = 5,
+                            cacheMaxCapacity: Int = 100,
+                            cacheTimeToLive: Duration = 10.minutes,
+                            cacheTimeToIdle: Duration = 9.minutes,
+                          )(
+                            implicit
+                              cachesupport: StoreSupport[VId,VType],
+                              execute: ExecutionContext
+                          ) extends Store[VId,VType]( new ZipPersistentSupport[VId,VType](zipfile),
+                                                           cacheInitialCapacity,
+                                                           cacheMaxCapacity,
+                                                           cacheTimeToLive,
+                                                           cacheTimeToIdle
+                                                         ) {
+
+}
