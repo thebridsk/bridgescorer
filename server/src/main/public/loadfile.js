523373fc4789302f01a7f01536b20264ddcd6e97
The Bridge Scorekeeper <35344976+thebridsk@users.noreply.github.com>
2019-01-19 21:38:26 -0500

    don't use document.write

---
+++ server/src/main/public/loadfile.js
@@ -0,0 +1,35 @@
+/**
+ * Get HTML asynchronously
+ * @param  {String}   url      The URL to get HTML from
+ * @param  {Function} callback A callback funtion. Pass in "response" variable to use returned HTML.
+ */
+var getHTML = function ( url, callback ) {
+
+    // Feature detection
+    if ( !window.XMLHttpRequest ) return;
+
+    // Create new request
+    var xhr = new XMLHttpRequest();
+
+    // Setup callback
+    xhr.onload = function() {
+        if ( callback && typeof( callback ) === 'function' ) {
+            callback( this.responseXML );
+        }
+    }
+
+    // Get the HTML
+    xhr.open( 'GET', url );
+    xhr.responseType = 'document';
+    xhr.send();
+
+};
+
+var getHTMLFragment = function( url, selector ) {
+    getHTML( url, function (response) {
+        var someElem = document.querySelector( selector );
+        var someOtherElem = response.querySelector( ':root > body > div' );
+        someElem.innerHTML = someOtherElem.innerHTML;
+    });
+    
+}
