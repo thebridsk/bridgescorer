ce877936773c4b8c0076246608b30dca9842ecda
The Bridge Scorekeeper <35344976+thebridsk@users.noreply.github.com>
2019-09-01 22:39:33 -0400

    toggle between light and dark theme

--- materialui/src/main/scala/com/github/thebridsk/materialui/icons/SvgIconBase.scala
+++ materialui/src/main/scala/com/github/thebridsk/materialui/icons/SvgIconBase.scala
@@ -127,8 +127,6 @@ trait SvgIconBase extends ComponentFactory[SvgIconProps] {
 
   protected val f: Js.Component[SvgIconProps, Null, CtorType.PropsAndChildren]
 
-  import js._
-
   /**
     * @param classes Node passed into the SVG element.
     * @param color The color of the component. It supports those theme colors
@@ -210,5 +208,73 @@ trait SvgIconBase extends ComponentFactory[SvgIconProps] {
       viewBox = viewBox,
       additionalProps = additionalProps
     )
+  }
+}
+
+
+object MuiSvgIcon extends ComponentFactory[SvgIconProps] {
+  @js.native @JSImport("@material-ui/core/SvgIcon", JSImport.Default) private object SvgIcon
+      extends js.Any
+
+  protected val f = JsComponent[SvgIconProps, Children.Varargs, Null](SvgIcon)
+
+  /**
+    * @param classes Node passed into the SVG element.
+    * @param color The color of the component. It supports those theme colors
+    *               that make sense for this component. You can use the
+    *               htmlColor property to apply a color attribute to the
+    *               SVG element.
+    *               Default: inherit
+    * @param component The component used for the root node. Either a string
+    *                   to use a DOM element or a component.
+    *                   Default: svg
+    * @param fontSize The fontSize applied to the icon. Defaults to 24px,
+    *                  but can be configure to inherit font size.
+    *                  Default: default
+    * @param htmlColor Applies a color attribute to the SVG element.
+    * @param shapeRendering The shape-rendering attribute. The behavior of the
+    *                        different options is described on the MDN Web Docs.
+    *                        If you are having issues with blurry icons you should
+    *                        investigate this property.
+    * @param titleAccess Provides a human-readable title for the element that
+    *                     contains it. https://www.w3.org/TR/SVG-access/#Equivalent
+    * @param viewBox Allows you to redefine what the coordinates without units mean
+    *                 inside an SVG element. For example, if the SVG element is
+    *                 500 (width) by 200 (height), and you pass viewBox="0 0 50 20",
+    *                 this means that the coordinates inside the SVG will go from
+    *                 the top left corner (0,0) to bottom right (50,20) and each
+    *                 unit will be worth 10px.
+    *                 Default: '0 0 24 24'
+    * @param additionalProps a dictionary of additional properties
+    *
+    * @param children Node passed into the SVG element.
+    */
+    def apply(
+      classes: js.UndefOr[js.Dictionary[String]] = js.undefined,
+      color: js.UndefOr[SvgColor] = js.undefined,
+      component: js.UndefOr[String] = js.undefined,
+      fontSize: js.UndefOr[SvgFontSize] = js.undefined,
+      htmlColor: js.UndefOr[String] = js.undefined,
+      shapeRendering: js.UndefOr[String] = js.undefined,
+      titleAccess: js.UndefOr[String] = js.undefined,
+      viewBox: js.UndefOr[String] = js.undefined,
+      additionalProps: js.UndefOr[js.Dictionary[js.Any]] = js.undefined
+  )(
+    children: CtorType.ChildArg*
+  ) = {
+    val p: SvgIconProps = SvgIconProps(
+      classes = classes,
+      color = color,
+      component = component,
+      fontSize = fontSize,
+      htmlColor = htmlColor,
+      shapeRendering = shapeRendering,
+      titleAccess = titleAccess,
+      viewBox = viewBox,
+      additionalProps = additionalProps
+    )
+
+    val x = f(p) _
+    x(children)
   }
 }
