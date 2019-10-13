151ddf410d143a37b37fcc952ace20e16fb2aaed
The Bridge Scorekeeper <35344976+thebridsk@users.noreply.github.com>
2019-10-13 12:19:36 -0400

    fix periodic msg in travis

---
+++ travis/cmdWithPeriodicMsg.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+SCRIPTDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+helpme()
+{
+  cat <<HELPMEHELPME
+Syntax: $0 cmd
+Syntax: $0 [-h|--help]
+
+Runs cmd writing a message every 5 minutes to stdout.
+
+HELPMEHELPME
+}
+
+if [[ "$1" == "-h" || "$1" == "--help" ]] ; then
+  helpme
+  exit 1
+fi
+
+while sleep 5s; do echo "====== $SECONDS seconds, building ======"; done &
+
+$*
+rc=$?
+
+kill %1
+
+exit $rc
