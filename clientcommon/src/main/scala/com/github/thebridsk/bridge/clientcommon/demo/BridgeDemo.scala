a5207a6b6c4d3f27dfc38997c4090ba06ea3c0b0
The Bridge Scorekeeper <35344976+thebridsk@users.noreply.github.com>
2020-04-29 18:50:51 -0400

    prep for sjs1

--- clientcommon/src/main/scala/com/github/thebridsk/bridge/clientcommon/demo/BridgeDemo.scala
+++ clientcommon/src/main/scala/com/github/thebridsk/bridge/clientcommon/demo/BridgeDemo.scala
@@ -11,23 +11,36 @@ import scala.scalajs.js
 object BridgeDemo {   // need to figure out how to use new way to call main
 
   def setDemo( demo: Boolean ) = {
-    val g = js.Dynamic.global.asInstanceOf[js.Dictionary[Boolean]]
-    g("demo") = demo
+    // val g = js.Dynamic.global.asInstanceOf[js.Dictionary[Boolean]]
+    // g("demo") = demo
+    js.Dynamic.global.demo = demo
   }
 
   def isDemo: Boolean = {
-    val g = js.Dynamic.global.asInstanceOf[js.Dictionary[Boolean]]
-    if ( g.contains("demo") ) {
-      g("demo")
+    // val g = js.Dynamic.global.asInstanceOf[js.Dictionary[Boolean]]
+    // if ( g.contains("demo") ) {
+    //   g("demo")
+    // } else {
+    //   false
+    // }
+    if (js.typeOf(js.Dynamic.global.demo) == "boolean") {
+      val f: Boolean = js.Dynamic.global.demo.asInstanceOf[Boolean]
+      f
     } else {
       false
     }
   }
 
   def demoLogLevel: String = {
-    val g = js.Dynamic.global.asInstanceOf[js.Dictionary[String]]
-    if ( g.contains("demoLogLevel") ) {
-      g("demoLogLevel")
+    // val g = js.Dynamic.global.asInstanceOf[js.Dictionary[String]]
+    // if ( g.contains("demoLogLevel") ) {
+    //   g("demoLogLevel")
+    // } else {
+    //   ""
+    // }
+    if (js.typeOf(js.Dynamic.global.demoLogLevel) == "string") {
+      val f: String = js.Dynamic.global.demoLogLevel.toString()
+      f
     } else {
       ""
     }
