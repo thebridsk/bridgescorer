3db4248da31defb6fbc2e34eee02985b18ac5ddd
The Bridge Scorekeeper <35344976+thebridsk@users.noreply.github.com>
2020-12-25 10:06:35 -0500

    better fix for fullscreen

--- clientcommon/src/main/scala/com/github/thebridsk/bridge/clientcommon/fullscreen/Fullscreen.scala
+++ clientcommon/src/main/scala/com/github/thebridsk/bridge/clientcommon/fullscreen/Fullscreen.scala
@@ -6,6 +6,7 @@ import org.scalajs.dom.raw.Document
 import org.scalajs.dom.document
 import com.github.thebridsk.utilities.logging.Logger
 import com.github.thebridsk.bridge.clientcommon.dispatcher.Listenable
+import scala.scalajs.js.annotation.JSName
 
 /**
   * Extended Document object with fullscreen functions.
@@ -21,16 +22,22 @@ trait DocumentFullscreen extends js.Object {
   val fullscreenEnabled: js.UndefOr[Boolean] = js.native
   val fullscreenElement: js.UndefOr[Element] = js.native
   def exitFullscreen(): js.Promise[Unit] = js.native
+  @JSName("exitFullscreen")
+  def exitFullscreenVal: js.Any = js.native
 
   // for Chrome 15-70
   val webkitFullScreenEnabled: js.UndefOr[Boolean] = js.native
   val webkitFullScreenElement: js.UndefOr[Element] = js.native
-  def webkitExitFullscreen(): js.Promise[Unit] = js.native
+  def webkitExitFullScreen(): js.Promise[Unit] = js.native
+  @JSName("webkitExitFullScreen")
+  def webkitExitFullScreenVal: js.Any = js.native
 
   // for iPad
   val webkitFullscreenEnabled: js.UndefOr[Boolean] = js.native
   val webkitFullscreenElement: js.UndefOr[Element] = js.native
-  // def webkitExitFullscreen(): js.Promise[Unit] = js.native
+  def webkitExitFullscreen(): js.Promise[Unit] = js.native
+  @JSName("webkitExitFullscreen")
+  def webkitExitFullscreenVal: js.Any = js.native
 }
 
 /**
@@ -46,10 +53,16 @@ trait DocumentFullscreen extends js.Object {
 trait DocumentElementFullscreen extends js.Object {
   // standard names
   def requestFullscreen(): js.Promise[Unit] = js.native
+  @JSName("requestFullscreen")
+  def requestFullscreenVal: js.Any = js.native
   // for Chrome 15-70
   def webkitRequestFullScreen(): js.Promise[Unit] = js.native
+  @JSName("webkitRequestFullScreen")
+  def webkitRequestFullScreenVal: js.Any = js.native
   // for iPad
   def webkitRequestFullscreen(): js.Promise[Unit] = js.native
+  @JSName("webkitRequestFullscreen")
+  def webkitRequestFullscreenVal: js.Any = js.native
 }
 
 object Values {
@@ -70,10 +83,91 @@ object Implicits {
   val log: Logger = Logger("bridge.Fullscreen")
 
   private lazy val (webkitFullscreenEnabled, fullscreenEnabled, ipadFullscreenEnabled) = {
+    xxxyyyzzz()
     val doc = document.asInstanceOf[DocumentFullscreen]
     (doc.webkitFullScreenEnabled.getOrElse(false),
      doc.fullscreenEnabled.getOrElse(false),
      doc.webkitFullscreenEnabled.getOrElse(false))
+  }
+
+  private def xxxyyyzzz() = {
+
+    def checkValueF(n: String, r: => ()=>js.Promise[Unit]) = {
+      try {
+        val v = r
+        if (js.isUndefined(v)) {
+          log.info(s"xxxyyyzzz.checkValueF: $n is undefined")
+        }
+        else {
+          val t = js.typeOf(v)
+          t match {
+            case "object" =>
+              log.info(s"xxxyyyzzz.checkValueF: $n type is $t: $v")
+            case "boolean" =>
+              log.info(s"xxxyyyzzz.checkValueF: $n type is $t: $v")
+            case _ =>
+              log.info(s"xxxyyyzzz.checkValueF: $n type is $t")
+          }
+        }
+
+      } catch {
+        case x: Exception =>
+          log.info(s"xxxyyyzzz.checkValueF: $n exception $x")
+      }
+    }
+
+    def checkValue(n: String, r: => js.Any) = {
+      try {
+        val v = r
+        if (js.isUndefined(v)) {
+          log.info(s"xxxyyyzzz.checkValue: $n is undefined")
+        }
+        else {
+          val t = js.typeOf(v)
+          t match {
+            case "object" =>
+              log.info(s"xxxyyyzzz.checkValue: $n type is $t: $v")
+            case "boolean" =>
+              log.info(s"xxxyyyzzz.checkValue: $n type is $t: $v")
+            case _ =>
+              log.info(s"xxxyyyzzz.checkValue: $n type is $t")
+          }
+        }
+      } catch {
+        case x: Exception =>
+          log.info(s"xxxyyyzzz.checkValue: $n exception $x")
+      }
+    }
+
+    val doc = document.asInstanceOf[DocumentFullscreen]
+    val body = document.body.asInstanceOf[DocumentElementFullscreen]
+
+    checkValue("exitFullscreen", doc.exitFullscreen _)
+    checkValueF("exitFullscreen", doc.exitFullscreen _)
+    checkValue("exitFullscreenVal", doc.exitFullscreenVal)
+    checkValue("fullscreenElement", doc.fullscreenElement)
+    checkValue("fullscreenEnabled", doc.fullscreenEnabled)
+    checkValue("requestFullscreen", body.requestFullscreen _)
+    checkValueF("requestFullscreen", body.requestFullscreen _)
+    checkValue("requestFullscreenVal", body.requestFullscreenVal)
+
+    checkValue("webkitExitFullscreen", doc.webkitExitFullscreen _)
+    checkValueF("webkitExitFullscreen", doc.webkitExitFullscreen _)
+    checkValue("webkitExitFullscreenVal", doc.webkitExitFullscreenVal)
+    checkValue("webkitFullscreenElement", doc.webkitFullscreenElement)
+    checkValue("webkitFullscreenEnabled", doc.webkitFullscreenEnabled)
+    checkValue("webkitRequestFullscreen", body.webkitRequestFullscreen _)
+    checkValueF("webkitRequestFullscreen", body.webkitRequestFullscreen _)
+    checkValue("webkitRequestFullscreenVal", body.webkitRequestFullscreenVal)
+
+    checkValue("webkitExitFullScreen", doc.webkitExitFullScreen _)
+    checkValueF("webkitExitFullScreen", doc.webkitExitFullScreen)
+    checkValue("webkitExitFullScreenVal", doc.webkitExitFullScreenVal)
+    checkValue("webkitFullScreenElement", doc.webkitFullScreenElement)
+    checkValue("webkitFullScreenEnabled", doc.webkitFullScreenEnabled)
+    checkValue("webkitRequestFullScreen", body.webkitRequestFullScreen _)
+    checkValueF("webkitRequestFullScreen", body.webkitRequestFullScreen _)
+    checkValue("webkitRequestFullScreenVal", body.webkitRequestFullScreenVal)
   }
 
   lazy val isFullscreenEnabled = fullscreenEnabled || webkitFullscreenEnabled || ipadFullscreenEnabled
@@ -91,15 +185,15 @@ object Implicits {
     def myFullscreenEnabled: Boolean = Implicits.isFullscreenEnabled
 
     def myFullscreenElement: js.UndefOr[Element] = {
-      if (webkitFullscreenEnabled) doc.webkitFullScreenElement
-      else if (fullscreenEnabled) doc.fullscreenElement
-      else if (ipadFullscreenEnabled) doc.webkitFullscreenElement
+      if (!js.isUndefined(doc.fullscreenEnabled)) doc.fullscreenElement
+      else if (!js.isUndefined(doc.webkitFullscreenElement)) doc.webkitFullscreenElement
+      else if (!js.isUndefined(doc.webkitFullScreenElement)) doc.webkitFullScreenElement
       else js.undefined
     }
     def myExitFullscreen(): js.Promise[Unit] = {
-      if (webkitFullscreenEnabled) doc.webkitExitFullscreen()
-      else if (fullscreenEnabled)doc.exitFullscreen()
-      else if (ipadFullscreenEnabled) doc.webkitExitFullscreen()
+      if (!js.isUndefined(doc.exitFullscreenVal)) doc.exitFullscreen()
+      else if (!js.isUndefined(doc.webkitExitFullscreenVal)) doc.webkitExitFullscreen()
+      else if (!js.isUndefined(doc.webkitExitFullScreenVal)) doc.webkitExitFullScreen()
       else js.Promise.reject("fullscreen not supported")
     }
 
@@ -122,9 +216,10 @@ object Implicits {
       element.asInstanceOf[DocumentElementFullscreen]
 
     def myRequestFullscreen(): js.Promise[Unit] = {
-      if (webkitFullscreenEnabled) elem.webkitRequestFullScreen()
-      else if (fullscreenEnabled) elem.requestFullscreen()
-      else if (ipadFullscreenEnabled) elem.webkitRequestFullscreen()
+      xxxyyyzzz()
+      if (!js.isUndefined(elem.requestFullscreenVal)) elem.requestFullscreen()
+      else if (!js.isUndefined(elem.webkitRequestFullscreenVal)) elem.webkitRequestFullscreen()
+      else if (!js.isUndefined(elem.webkitRequestFullScreenVal)) elem.webkitRequestFullScreen()
       else js.Promise.reject("fullscreen not supported")
     }
 
